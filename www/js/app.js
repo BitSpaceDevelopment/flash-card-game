45ccc4688394e8569786373b25c8b0301c0c63c6
Dann Blair <dann@bitspacedevelopment.com>
2015-07-23 19:47:28 -0500

    Parse is officially working

---
+++ www/js/app.js
@@ -0,0 +1,7 @@
+Parse.initialize("YOUR_APP_KEY", "YOUR_SECRET");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
